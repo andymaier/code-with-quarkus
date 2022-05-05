2a4d45fee56ab2efc4244de4cd89879f28ac6c24
Andreas Maier <a.maier@systems62.com>
2022-05-05 09:16:47 +0200

    initial commit, has jax rs and orm hibternate

---
+++ src/test/java/com/systems62/GreetingResourceIT.java
@@ -0,0 +1,9 @@
+package com.systems62;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
